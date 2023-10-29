e1941479b1297337ab6f0c229bc4c0c0f566955c
ouhammmourachid <rachidouhammou21@gmail.com>
2023-10-29 09:44:52 +0100

    fix bugs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 echo "start bumpversion exec"
 bumpversion $1
+
+git config --global user.email "bumpversion@github-actions"
+git config --global user.name "BumpVersion Action"
+
 new_version=$(git describe --tags --abbrev=0)
 echo $new_version
 echo "new_version=$new_version" >> $GITHUB_OUTPUT
